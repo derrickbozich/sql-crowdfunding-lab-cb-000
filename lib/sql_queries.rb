3823a8d177922fa7aeac5b143cc1d4776d9e21c7
derrick b <dbozich1@gmail.com>
2018-06-18 02:09:53 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -25,7 +25,6 @@ end
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
   "SELECT projects.title, SUM(pledges.amount)-projects.funding_goal AS amount_over
   FROM projects
-  WHERE amount_over >= 0
   INNER JOIN pledges
   ON projects.id = pledges.project_id
   GROUP BY projects.title;"
